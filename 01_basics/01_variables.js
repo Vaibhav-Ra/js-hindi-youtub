1efcad4512c16f1bc893111517f2e923005d0540
Vaibhav-Ra <63895306+Vaibhav-Ra@users.noreply.github.com>
2025-04-12 16:20:07 +0000

    conversion.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId=14453                     //No matter what this can't be altered
+let accountEmail = "vaibhav@google.com"
+var accountCity = "Jaipur"
+let accountState;
+
+
+
+// accountId=23  not allowed
+
+// please avoid using of var as much as you can
+console.log(accountCity)
+/*
+accountId=543
+accountEmail="vr@google.com"
+accountCity="Bangalore"
+*/
+
+//{} //These curly braces are called as scope.
+/*
+Prefer not to use var block of issue in block scope functional scope
+*/
+console.table([accountId,accountCity, accountEmail])
+
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat code as newer version
+
+//alert(7) //we are using nodejs not browser
+
+let name="vaibhav"
+let age="27"
+let isLogeddIn= false
+
+//number => 2 to the power 53
+//bigint 
+//string= " "
+// null = standalone alone
+// symbol =>unique
+
+//object
+
+//console.log(typeof "vaibhav");
+//console.log(typeof 76);
+//console.log(typeof 23.76);
+//console.log(typeof null);
+//console.log(typeof undefined);
